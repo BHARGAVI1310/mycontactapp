34b2854d7a01cd86ac604f91746165721b8ed5fe
kotha <kotha@LAPTOP-O8F3G305>
2024-07-11 16:21:05 +0530

    Welcome class is updated

--- src/mycontactapp/Welcome.java
+++ src/mycontactapp/Welcome.java
@@ -1,5 +1,8 @@
 package mycontactapp;
 
 public class Welcome {
-
+   public static void main(String[] args) {
+	    System.out.println("welcome to github!");
+	System.out.println("Git Tool");
+}
 }
